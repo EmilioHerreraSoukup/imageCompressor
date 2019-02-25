93e2cf1f5286b0679c02d5ec1783bd452448a5e0
Emilio Herrera <emilioherrerasoukup@gmail.com>
2019-02-25 17:35:18 +0100

    Initial commit

---
+++ compressImgs.js
@@ -0,0 +1,106 @@
+const imagemin = require("imagemin");
+const imageminGifsicle = require("imagemin-gifsicle");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminSVG = require("imagemin-svgo");
+const fs = require("fs-extra");
+const path = require("path");
+
+let inDir, outDir;
+
+inDir = "./images";
+outDir = "./compressed";
+
+fs.removeSync(outDir);
+
+console.log("Processing images...\n");
+
+imagemin([`${inDir}/*.{jpg,png,gif,svg,webp}`], outDir, {
+  plugins: [
+    imageminMozjpeg({ quality: "80" }),
+    imageminPngquant({ quality: "65-80" }),
+    imageminGifsicle({ optimizationLevel: 3 }),
+    imageminSVG({
+      plugins: [
+        {
+          cleanupAttrs: true,
+          inlineStyles: true,
+          removeDoctype: true,
+          removeXMLProcInst: true,
+          removeComments: true,
+          removeMetadata: true,
+          removeTitle: true,
+          removeDesc: true,
+          removeUselessDefs: true,
+          removeXMLNS: false,
+          removeEditorsNSData: true,
+          removeEmptyAttrs: true,
+          removeHiddenElems: true,
+          removeEmptyText: true,
+          removeEmptyContainers: true,
+          removeViewBox: false,
+          cleanupEnableBackground: true,
+          minifyStyles: true,
+          convertStyleToAttrs: true,
+          convertColors: true,
+          convertPathData: true,
+          convertTransform: true,
+          removeUnknownsAndDefaults: true,
+          removeNonInheritableGroupAttrs: true,
+          removeUselessStrokeAndFill: true,
+          removeUnusedNS: true,
+          cleanupIDs: true,
+          cleanupNumericValues: true,
+          cleanupListOfValues: true,
+          moveElemsAttrsToGroup: true,
+          moveGroupAttrsToElems: true,
+          collapseGroups: true,
+          removeRasterImages: true,
+          mergePaths: true,
+          convertShapeToPath: true,
+          sortAttrs: true,
+          removeDimensions: true,
+          removeAttrs: true,
+          removeElementsByAttr: true,
+          addClassesToSVGElement: true,
+          addAttributesToSVGElement: true,
+          removeStyleElement: false,
+          removeScriptElement: true
+        }
+      ]
+    })
+  ]
+}).then(files => {
+  console.log("Image Optimization Results");
+  console.log("==============================");
+  logStats(files);
+});
+
+function logStats(files) {
+  let totalSaved = 0;
+  files.forEach(function(obj) {
+    let stats = getStats(obj.path);
+    console.log(
+      Math.round(((stats.oldSize - stats.newSize) / stats.oldSize) * 100) + "%",
+      stats.file
+    );
+    totalSaved += stats.oldSize - stats.newSize;
+  });
+  console.log("======================\n", btokb(totalSaved) + " Saved");
+  console.log("", "Files written to " + outDir);
+}
+
+function getStats(file) {
+  let fileName = path.parse(file).base;
+  let fileStats = fs.statSync(file);
+  let oldFileStats = fs.statSync(path.join(inDir, fileName));
+  return {
+    file: fileName,
+    oldSize: oldFileStats["size"],
+    newSize: fileStats["size"]
+  };
+}
+
+function btokb(val) {
+  return (val / 1000).toFixed(1) + "kb";
+}
