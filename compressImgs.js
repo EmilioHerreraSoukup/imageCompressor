5c4a3e7ca3faf205d56d718a4aab4048b37bb0e6
Emilio Herrera Soukup <emilio.herrera@accolade.com>
2023-02-06 14:35:01 +0100

    update

--- compressImgs.js
+++ compressImgs.js
@@ -1,10 +1,10 @@
-const imagemin = require("imagemin");
-const imageminGifsicle = require("imagemin-gifsicle");
-const imageminMozjpeg = require("imagemin-mozjpeg");
-const imageminPngquant = require("imagemin-pngquant");
-const imageminSVG = require("imagemin-svgo");
-const fs = require("fs-extra");
-const path = require("path");
+import imagemin from "imagemin";
+import imageminGifsicle from "imagemin-gifsicle";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminPngquant from "imagemin-pngquant";
+import imageminSVG from "imagemin-svgo";
+import fs from "fs-extra";
+import path from "path";
 
 let inDir, outDir;
 
@@ -21,61 +21,70 @@ imagemin([`${inDir}/*.{jpg,png,gif,svg,webp}`], {
   destination: outDir,
   plugins: [
     imageminMozjpeg({ quality: 80 }),
-    imageminPngquant({ quality: [0.65,0.8] }),
+    imageminPngquant({ quality: [0.65, 0.8] }),
     imageminGifsicle({ optimizationLevel: 3 }),
     imageminSVG({
       plugins: [
         {
-          cleanupAttrs: true,
-          inlineStyles: true,
-          removeDoctype: true,
-          removeXMLProcInst: true,
-          removeComments: true,
-          removeMetadata: true,
-          removeTitle: true,
-          removeDesc: true,
-          removeUselessDefs: true,
-          removeXMLNS: false,
-          removeEditorsNSData: true,
-          removeEmptyAttrs: true,
-          removeHiddenElems: true,
-          removeEmptyText: true,
-          removeEmptyContainers: true,
-          removeViewBox: false,
-          cleanupEnableBackground: true,
-          minifyStyles: true,
-          convertStyleToAttrs: true,
-          convertColors: true,
-          convertPathData: true,
-          convertTransform: true,
-          removeUnknownsAndDefaults: true,
-          removeNonInheritableGroupAttrs: true,
-          removeUselessStrokeAndFill: true,
-          removeUnusedNS: true,
-          cleanupIDs: true,
-          cleanupNumericValues: true,
-          cleanupListOfValues: true,
-          moveElemsAttrsToGroup: true,
-          moveGroupAttrsToElems: true,
-          collapseGroups: true,
-          removeRasterImages: true,
-          mergePaths: true,
-          convertShapeToPath: true,
-          sortAttrs: true,
-          removeDimensions: true,
-          removeAttrs: true,
-          removeElementsByAttr: true,
-          addClassesToSVGElement: true,
-          addAttributesToSVGElement: true,
-          removeStyleElement: false,
-          removeScriptElement: true
-        }
-      ]
-    })
-  ]
-}).then(files => {
-
-  
+          name: "sortAttrs",
+          params: {
+            xmlnsOrder: "alphabetical",
+          },
+        },
+        {
+          name: "preset-default",
+          params: {
+            overrides: {
+              cleanupAttrs: true,
+              inlineStyles: true,
+              removeDoctype: true,
+              removeXMLProcInst: true,
+              removeComments: true,
+              removeMetadata: true,
+              removeTitle: true,
+              removeDesc: true,
+              removeUselessDefs: true,
+              removeXMLNS: false,
+              removeEditorsNSData: true,
+              removeEmptyAttrs: true,
+              removeHiddenElems: true,
+              removeEmptyText: true,
+              removeEmptyContainers: true,
+              removeViewBox: false,
+              cleanupEnableBackground: true,
+              minifyStyles: true,
+              convertStyleToAttrs: true,
+              convertColors: true,
+              convertPathData: true,
+              convertTransform: true,
+              removeUnknownsAndDefaults: true,
+              removeNonInheritableGroupAttrs: true,
+              removeUselessStrokeAndFill: true,
+              removeUnusedNS: true,
+              cleanupIDs: true,
+              cleanupNumericValues: true,
+              cleanupListOfValues: true,
+              moveElemsAttrsToGroup: true,
+              moveGroupAttrsToElems: true,
+              collapseGroups: true,
+              removeRasterImages: true,
+              mergePaths: true,
+              convertShapeToPath: true,
+              sortAttrs: true,
+              removeDimensions: true,
+              removeAttrs: true,
+              removeElementsByAttr: true,
+              addClassesToSVGElement: true,
+              addAttributesToSVGElement: true,
+              removeStyleElement: false,
+              removeScriptElement: true,
+            },
+          },
+        },
+      ],
+    }),
+  ],
+}).then((files) => {
   console.log("Image Optimization Results");
   console.log("==============================");
   logStats(files);
@@ -83,9 +92,7 @@ imagemin([`${inDir}/*.{jpg,png,gif,svg,webp}`], {
 
 function logStats(files) {
   let totalSaved = 0;
-  files.forEach(function(obj) {
-
-
+  files.forEach(function (obj) {
     let stats = getStats(obj);
     console.log(
       Math.round(((stats.oldSize - stats.newSize) / stats.oldSize) * 100) + "%",
@@ -93,21 +100,22 @@ function logStats(files) {
     );
     totalSaved += stats.oldSize - stats.newSize;
   });
-  console.log("===============================\n", btokb(totalSaved) + " Saved");
+  console.log(
+    "===============================\n",
+    btokb(totalSaved) + " Saved"
+  );
   console.log("", "Files written to " + outDir);
 }
 
 function getStats(file) {
-
   let fileName = path.parse(file.sourcePath).base;
   let fileStats = fs.statSync(file.destinationPath);
   let oldFileStats = fs.statSync(path.join(inDir, fileName));
 
-
   return {
     file: fileName,
     oldSize: oldFileStats["size"],
-    newSize: fileStats["size"]
+    newSize: fileStats["size"],
   };
 }
 
